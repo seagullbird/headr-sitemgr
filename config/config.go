22caf3c37138f26f0ddf7dcee1cadbb59c5f9ab5
seagullbird <seagullbird823@gmail.com>
2018-03-29 10:34:18 +0800

    changed default theme

--- config/config.go
+++ config/config.go
@@ -4,7 +4,7 @@ var (
 	// PORT is the serving port, this value is set during compile time
 	PORT = "unset"
 	// InitialTheme currently is set dead to gohugo-theme-ananke
-	InitialTheme = "gohugo-theme-ananke"
+	InitialTheme = "hugo-theme-cactus-plus"
 	// Auth0Domain is the domain for my auth0 service
 	Auth0Domain = "https://headr.auth0.com"
 	// Auth0Audience is the identifier for Auth0 API 'headr-api'
